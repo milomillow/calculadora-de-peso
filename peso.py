8cbf3144e570c60c593e597ad915fefe6e39e236
Milo <perrut@proton.me>
2025-09-14 22:29:38 -0300

    Add files via upload

---
+++ peso.py
@@ -0,0 +1,15 @@
+# Calculadora de peso
+
+import time
+import os
+os.system('clear')
+print("Calculadora de peso")
+print('Desenvolvido por MilioMilow')
+time.sleep(2)
+os.system('clear')
+nome =input("Digite seu nome: ")
+peso =float(input("Digite seu peso em kg: "))
+os.system('clear')
+# ...existing code...
+print(f"Olá {nome}, seu peso é {peso} kg")
+# ...existing code...
